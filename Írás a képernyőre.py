ce711c073f2761c95157954237fee25151a924cf
jeneigabor <e15.jenei.gabor@palfy-vizugyi.hu>
2017-01-26 17:27:21 +0100

    Add files via upload

---
+++ Írás a képernyőre.py
@@ -0,0 +1,6 @@
+print("Szia")
+#print('...Hogyha én ezt tudtam volna) --- nem fogja kiírni, mert nincs lezárva ' -lel
+print('...Hogyha én ezt tudtam volna') #--- így helyes
+print('Köpönyeget hoztam vóna'!)
+print('Rásír a sok kis malac is!')
+print('\"Én is, én is, én is, én is!\"')
