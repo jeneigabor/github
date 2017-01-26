01587fe3624ca4f1e6c1015bc041d501f42975bf
jeneigabor <e15.jenei.gabor@palfy-vizugyi.hu>
2017-01-26 17:58:53 +0100

    Add files via upload

---
+++ Változók.py
@@ -0,0 +1,56 @@
+refrén = 'Csillárom, haj!'
+print('Csizmadialegények')
+print('\nCsizmadialegények')
+print(refrén) 
+print('Nyolcan ülnek egy lóra')
+print(refrén)
+print('Úgy mennek a jó borra')
+print(refrén)
+# a következő sorban felülírjuk a változó értékét
+refrén = 'Sej, ripityom!'
+print('Parancsolnak kocsmárosnak;')
+print('Mérjen egy decit nyócuknak!')
+print(refrén) # itt már Sej, ripityom! lesz
+print('Azt se tudják kifizetni.')
+print('Az ablakon szekdesnek ki.')
+print(refrén)
+
+
+
+
+#Feladatok
+#1. feladat megoldása: az "egért nem fogja kiírni"
+cica = 'kutya'
+kutya = 'egér'
+egér = 'cica'
+print(cica)
+print('kutya')
+print(egér)
+print('cica')
+
+#2. feladat megoldása: a "tyúk" lesz kiírva
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+
+#3. feladat megoldása: 11 sor
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi?\n\n')
+print('Utolsó sor')
+
+#4. feladat megoldása:
+print("    /\n   /\n  /\n /\n/")
+#5. feladat megoldása:
+print("\\\n \\\n  \\\n   \\\n    \\\n     \\")
+
+#6. feladat megoldása:
+print("\\    /\n \\  /\n  \\/")
